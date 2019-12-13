51bbce424d3bc99e7645f8ce4437bd78841a8068
levelKro <36101908+levelKro@users.noreply.github.com>
2019-12-13 15:50:54 -0500

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -3,10 +3,19 @@ clear
 echo "### Installation du FFMpeg pour CentOS 7"
 echo " "
 echo " "
+read -p "### Appuyer sur [ENTRER] pour commencer... (ou CTRL+C pour annuler)"
 
-read -p "### Appuyer sur [ENTRER] pour commencer..."
+while true; do
+    read -p "Avez-vous besoin d'installer le Repository Forge ?" ynrpm
+    case $ynrpm in
+        [YyOo]* ) 
 echo "### Installation du RPM RepoForge"
-#rpm -Uhv --quiet http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
+rpm -Uhv --quiet http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
+			break;;
+        [Nn]* ) break;;
+        * ) echo "Entrer Y pour oui ou N pour non.";;
+    esac
+done
 
 echo "### Actualisation du systeme"
 yum -y -q update
@@ -23,7 +32,10 @@ yum -y -q install ncurses-devel
 yum -y -q install libdc1394 libdc1394-devel
 yum -y -q install amrnb-devel amrwb-devel opencore-amr-devel 
 
-read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer..."
+while true; do
+    read -p "Besoin de compiler codec Lame MP3 ?" ynmp3
+    case $ynmp3 in
+        [YyOo]* ) 
 echo "### Librairie MP3 Lame"
 cd /opt  
 wget http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz 
@@ -33,9 +45,17 @@ cd lame-3.99.5
 ./configure
 make -s  
 make -s install  
-make clean
-read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer..."
-
+make -s clean
+read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer... (ou CTRL+C pour annuler)"
+			break;;
+        [Nn]* ) break;;
+        * ) echo "Entrer Y pour oui ou N pour non.";;
+    esac
+done
+while true; do
+    read -p "Besoin de compiler codec XViD ?" ynxvid
+    case $ynxvid in
+        [YyOo]* ) 
 echo "### Librairie XVID"
 cd /opt
 wget http://downloads.xvid.org/downloads/xvidcore-1.3.2.tar.gz
@@ -45,9 +65,17 @@ cd xvidcore/build/generic
 ./configure --prefix="$HOME/ffmpeg_build"
 make -s
 make -s install	
-make clean 
-read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer..."
-
+make -s clean 
+read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer... (ou CTRL+C pour annuler)"
+			break;;
+        [Nn]* ) break;;
+        * ) echo "Entrer Y pour oui ou N pour non.";;
+    esac
+done
+while true; do
+    read -p "Besoin de compiler codec OGG ?" ynogg
+    case $ynogg in
+        [YyOo]* ) 
 echo "### Librairie OGG"
 cd /opt
 wget http://downloads.xiph.org/releases/ogg/libogg-1.3.1.tar.gz
@@ -57,9 +85,17 @@ cd libogg-1.3.1
 ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
 make -s
 make -s install
-make clean
-read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer..."
-
+make -s clean
+read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer... (ou CTRL+C pour annuler)"
+			break;;
+        [Nn]* ) break;;
+        * ) echo "Entrer Y pour oui ou N pour non.";;
+    esac
+done
+while true; do
+    read -p "Besoin de compiler codec Vorbis ?" ynvorbis
+    case $ynvorbis in
+        [YyOo]* ) 
 echo "### Librairie Vorbis"
 cd /opt
 wget http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.gz
@@ -69,9 +105,17 @@ cd libvorbis-1.3.4
 ./configure --prefix="$HOME/ffmpeg_build" --with-ogg="$HOME/ffmpeg_build" --disable-shared
 make -s
 make -s install
-make clean
-read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer..."
-
+make -s clean
+read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer... (ou CTRL+C pour annuler)"
+			break;;
+        [Nn]* ) break;;
+        * ) echo "Entrer Y pour oui ou N pour non.";;
+    esac
+done
+while true; do
+    read -p "Besoin de compiler codec Theora ?" yntheora
+    case $yntheora in
+        [YyOo]* ) 
 echo "### Librairie Theora"
 cd /opt
 wget http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.gz
@@ -81,9 +125,17 @@ cd libtheora-1.1.1
 ./configure --prefix="$HOME/ffmpeg_build" --with-ogg="$HOME/ffmpeg_build" --disable-examples --disable-shared --disable-sdltest --disable-vorbistest
 make -s
 make -s install
-make clean
-read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer..."
-
+make -s clean
+read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer... (ou CTRL+C pour annuler)"
+			break;;
+        [Nn]* ) break;;
+        * ) echo "Entrer Y pour oui ou N pour non.";;
+    esac
+done
+while true; do
+    read -p "Besoin de compiler codec AAC ?" ynaac
+    case $ynaac in
+        [YyOo]* ) 
 echo "### Librairie AAC"
 cd /opt
 wget http://downloads.sourceforge.net/opencore-amr/vo-aacenc-0.1.2.tar.gz
@@ -93,9 +145,17 @@ cd vo-aacenc-0.1.2
 ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
 make -s
 make -s install
-make clean
-read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer..."
-
+make -s clean
+read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer... (ou CTRL+C pour annuler)"
+			break;;
+        [Nn]* ) break;;
+        * ) echo "Entrer Y pour oui ou N pour non.";;
+    esac
+done
+while true; do
+    read -p "Besoin de compiler codec FAAC ?" ynfaac
+    case $ynfaac in
+        [YyOo]* ) 
 echo "### Librairie FAAC"
 cd /opt
 wget http://downloads.sourceforge.net/faac/faac-1.28.tar.gz
@@ -107,9 +167,17 @@ cd faac-1.28
 make -s
 make -s install
 ldconfig
-make clean
-read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer..."
-
+make -s clean
+read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer... (ou CTRL+C pour annuler)"
+			break;;
+        [Nn]* ) break;;
+        * ) echo "Entrer Y pour oui ou N pour non.";;
+    esac
+done
+while true; do
+    read -p "Besoin de compiler codec Yasm ?" ynyasm
+    case $ynyasm in
+        [YyOo]* ) 
 echo "### Librairie YASM"
 yum remove yasm
 cd /opt
@@ -121,9 +189,17 @@ cd yasm-1.2.0
 make -s
 make -s install
 export "PATH=$PATH:$HOME/bin"
-make clean
-read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer..." 
-
+make -s clean
+read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer... (ou CTRL+C pour annuler)" 
+			break;;
+        [Nn]* ) break;;
+        * ) echo "Entrer Y pour oui ou N pour non.";;
+    esac
+done
+while true; do
+    read -p "Besoin de compiler codec VPx ?" ynvpx
+    case $ynvpx in
+        [YyOo]* ) 
 echo "### Librairie VPx"
 cd /opt
 git clone https://chromium.googlesource.com/webm/libvpx.git
@@ -132,23 +208,36 @@ git checkout tags/v.1.3.0
 ./configure --prefix="$HOME/ffmpeg_build" --disable-examples
 make -s
 make -s install
-make clean
-read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer..."
-
+make -s clean
+read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer... (ou CTRL+C pour annuler)"
+			break;;
+        [Nn]* ) break;;
+        * ) echo "Entrer Y pour oui ou N pour non.";;
+    esac
+done
+while true; do
+    read -p "Besoin de compiler codec x264 ?" ynx264
+    case $ynx264 in
+        [YyOo]* ) 
 echo "### Librairie x264"
 cd /opt
 git clone git://git.videolan.org/x264.git
 cd x264
-./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static 
+#WARNING: "--disable-asm" est du a une erreur, le nasm présent sous CentOS7 est 2.10 mais la version 3.10 est requise, doit installer idéalement un nasm plus récent sans yum
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --disable-asm
 make -s
 make -s install
-make clean
-read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer..."
+make -s clean
+read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer... (ou CTRL+C pour annuler)"
+			break;;
+        [Nn]* ) break;;
+        * ) echo "Entrer Y pour oui ou N pour non.";;
+    esac
+done
 
 export LD_LIBRARY_PATH=/usr/local/lib/
 echo /usr/local/lib >> /etc/ld.so.conf.d/custom-libs.conf
 ldconfig
-read -p "### Pas d'erreur ? Appuyer sur [ENTRER] pour continuer..."
 
 echo "### Encodage du FFMpeg"
 cd /opt
@@ -160,5 +249,5 @@ export PKG_CONFIG_PATH
 ./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="$HOME/bin" --extra-libs=-ldl --enable-version3 --enable-libvpx --enable-libfaac --enable-libmp3lame --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libvo-aacenc --enable-libxvid --disable-ffplay --enable-gpl --enable-postproc --enable-nonfree --enable-avfilter --enable-pthreads
 make -s
 make -s install
-make clean
+make -s clean
 echo "### Installation complete"
