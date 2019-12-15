4cbcf7fcf683f673f01e2902d5e94c7605baa2b4
levelKro <36101908+levelKro@users.noreply.github.com>
2019-12-15 18:29:19 -0500

    Add Vorbis
    Add vorbis and fixes

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -35,38 +35,38 @@ mkdir ~/ffmpeg_sources
 
 echo "### 2.1 Nasm 2.14"
 cd ~/ffmpeg_sources
-curl -s -O -L https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.bz2
+curl --quiet  -O -L https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.bz2
 tar xjf nasm-2.14.02.tar.bz2
 cd nasm-2.14.02
 ./autogen.sh
 ./configure --quiet --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
-make -s
-make -s install
+make --quiet 
+make --quiet  install
 
 echo "### Étape 2.2 Yasm 1.3"
 cd ~/ffmpeg_sources
-curl -s -O -L https://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
+curl --quiet  -O -L https://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
 tar xzf yasm-1.3.0.tar.gz
 cd yasm-1.3.0
 ./configure --quiet --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
-make -s
-make -s install
+make --quiet 
+make --quiet  install
 
 echo "### Étape 2.2 Codec x264 (latest)"
 cd ~/ffmpeg_sources
 git --quiet clone --depth 1 https://code.videolan.org/videolan/x264.git
 cd x264
 PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --quiet --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
-make -s 
-make -s install
+make --quiet  
+make --quiet  install
 
 echo "### Étape 2.3 Codec x265 (latest)"
 cd ~/ffmpeg_sources
 hg -q clone https://bitbucket.org/multicoreware/x265
 cd ~/ffmpeg_sources/x265/build/linux
 cmake --no-warn-unused-cli -Wno-dev -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
-make -s
-make -s install
+make --quiet 
+make --quiet  install
 
 echo "### Étape 2.4 Codec AAC (latest)"
 cd ~/ffmpeg_sources
@@ -74,41 +74,50 @@ git --quiet clone --depth 1 https://github.com/mstorsjo/fdk-aac
 cd fdk-aac
 autoreconf -fiv
 ./configure --quiet --prefix="$HOME/ffmpeg_build" --disable-shared
-make -s
-make -s install
+make --quiet 
+make --quiet  install
 
 echo "### Étape 2.5 Codec Lame 3.100"
 cd ~/ffmpeg_sources
-curl -s -O -L https://downloads.sourceforge.net/project/lame/lame/3.100/lame-3.100.tar.gz
+curl --quiet  -O -L https://downloads.sourceforge.net/project/lame/lame/3.100/lame-3.100.tar.gz
 tar xzf lame-3.100.tar.gz
 cd lame-3.100
 ./configure --quiet --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm
-make -s
-make -s install
+make --quiet 
+make --quiet  install
 
 echo "### Étape 2.6 Codec Opus 1.3.1"
 cd ~/ffmpeg_sources
-curl -s -O -L https://archive.mozilla.org/pub/opus/opus-1.3.1.tar.gz
+curl --quiet  -O -L https://archive.mozilla.org/pub/opus/opus-1.3.1.tar.gz
 tar xzf opus-1.3.1.tar.gz
 cd opus-1.3.1
 ./configure --quiet --prefix="$HOME/ffmpeg_build" --disable-shared
-make -s
-make -s install
+make --quiet 
+make --quiet  install
 
 echo "### Étape 2.7 Codec VPX (latest)"
 cd ~/ffmpeg_sources
 git --quiet clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
 cd libvpx
 ./configure --quiet --prefix="$HOME/ffmpeg_build" --disable-examples --disable-unit-tests --enable-vp9-highbitdepth --as=yasm
-make -s
-make -s install
+make --quiet 
+make --quiet  install
+
+echo "### Étape 2.8 Codec Vorbis 1.3.5"
+cd ~/ffmpeg_sources
+wget -q http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.gz
+tar xzf libvorbis-1.3.5.tar.gz
+cd libvorbis-1.3.5
+./configure --quiet --prefix="$HOME/ffmpeg_build" --with-ogg="$HOME/ffmpeg_build" --disable-shared
+make --quiet 
+make --quiet  install
 
 echo "### Étape 2 complété : Librairies compilés, si aucune erreur c'est produite, vous pouvez continuer avec la compilation de FFMpeg."
 read -p "### Appuyer sur [ENTRER] pour continuer... (ou CTRL+C pour annuler)"
 
 echo "### Étape 3 : Compilation de FFMpeg (latest), ceci peut être (très) long..."
 cd ~/ffmpeg_sources
-curl -s -O -L https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
+curl --quiet  -O -L https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
 tar xjf ffmpeg-snapshot.tar.bz2
 cd ffmpeg
 PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
@@ -124,13 +133,14 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
   --enable-libfreetype \
   --enable-libmp3lame \
   --enable-libopus \
+  --enable-libvorbis \
   --enable-libvpx \
   --enable-libx264 \
   --enable-libx265 \
-  --enable-nonfree
+  --enable-nonfree \
   --quiet
-make -s
-make -s install
+make --quiet 
+make --quiet  install
 hash -d ffmpeg
 
 echo "### Installation terminé, faites 'ffmpeg -v' pour vérifier si il est correctement installé."
